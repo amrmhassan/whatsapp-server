5ffb937a35c8c6d7a9b08a0902010fe0321392af
Amr Hassan <amrhassanpersonal@gmail.com>
2024-02-22 08:01:20 +0200

    sending user cron job done

--- lib/features/sockets/data/datasources/socket_datasource.dart
+++ lib/features/sockets/data/datasources/socket_datasource.dart
@@ -1,11 +1,14 @@
+import 'package:whatsapp_server/features/cron_job/data/datasources/cron_job_executer.dart';
 import 'package:whatsapp_server/init/runtime_variables.dart';
 import 'package:whatsapp_shared_code/whatsapp_shared_code/models/socket_data_model.dart';
 
 class SocketsDatasource {
-  void setUserId(SocketDataModel dataModel) {
+  final CronJobExecuter _jobExecuter = CronJobExecuter();
+  Future<void> setUserId(SocketDataModel dataModel) async {
     String userId = dataModel.body['userId'];
     String sessionId = dataModel.body['sessionId'];
-    socketManager.updateUserId(sessionId, userId);
+    await socketManager.updateUserId(sessionId, userId);
     print('Setting user id $userId to $sessionId');
+    await _jobExecuter.handleUserCronJobs(userId);
   }
 }
